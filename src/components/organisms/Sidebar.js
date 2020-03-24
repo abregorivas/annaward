ea380017073f5a8e4cbae7b76e5406c4b203a8f4
abregorivas <abregorivas@gmail.com>
2020-03-24 08:19:57 -0700

    add sidebar

---
+++ src/components/organisms/Sidebar.js
@@ -0,0 +1,64 @@
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import Drawer from "@material-ui/core/Drawer"
+import List from "@material-ui/core/List"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemText from "@material-ui/core/ListItemText"
+
+const drawerWidth = 240
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    paddingTop: theme.spacing(10),
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+}))
+
+export const Sidebar = () => {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.toolbar} />
+        <List>
+          {[
+            "Home",
+            "Biography",
+            "Projects",
+            "Choreography",
+            "History",
+            "Gallery",
+            "Revuews",
+            "Video",
+            "Contact",
+            "Links",
+          ].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </div>
+  )
+}
