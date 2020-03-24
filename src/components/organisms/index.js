ed135ce19377e2b3442288a584b06423073b4a41
abregorivas <abregorivas@gmail.com>
2020-03-24 14:51:24 -0700

    grid & organisms export file

---
+++ src/components/organisms/index.js
@@ -0,0 +1,8 @@
+// export { CorePrinciples } from "./CorePrinciples"
+// export { About } from './About'
+// export { Expertise } from './Expertise'
+// export { Services} from './Services'
+// export  { Clients } from './Clients'
+export { Banner } from "./Banner"
+export { Sidebar } from "./Sidebar"
+export { Footer } from "./Footer"
