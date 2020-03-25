9cd01c0d772a6efd3f5ebd42cb778bb5d61cafee
abregorivas <abregorivas@gmail.com>
2020-03-25 01:40:44 -0700

    links section added

--- src/components/images/linksImages.js
+++ src/components/images/linksImages.js
@@ -5,28 +5,63 @@ import Img from "gatsby-image"
 export const SiteImg = ({ altText, imgIndex, height }) => {
   const data = useStaticQuery(graphql`
     query {
-      img1: file(relativePath: { eq: "HISTORY/krassovska184.jpg" }) {
+      img1: file(relativePath: { eq: "LINKS/backstagedancewear.webp" }) {
         childImageSharp {
           fluid(maxWidth: 600) {
             ...GatsbyImageSharpFluid
           }
         }
       }
-      img2: file(relativePath: { eq: "HISTORY/KrassovskaSEYMOUR.jpg" }) {
+      img2: file(relativePath: { eq: "LINKS/galveston.png" }) {
         childImageSharp {
           fluid(maxWidth: 600) {
             ...GatsbyImageSharpFluid
           }
         }
       }
-      img3: file(relativePath: { eq: "HISTORY/krassovskaSigned.jpg" }) {
+      img3: file(relativePath: { eq: "LINKS/EDAS.png" }) {
         childImageSharp {
           fluid(maxWidth: 600) {
             ...GatsbyImageSharpFluid
           }
         }
       }
-      img4: file(relativePath: { eq: "HISTORY/nathalie_anna_flowers.jpg" }) {
+      img4: file(relativePath: { eq: "LINKS/mcballet_logo.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 600) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      img5: file(relativePath: { eq: "LINKS/nili_design_small.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 600) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      img6: file(relativePath: { eq: "LINKS/SIBA_web_logo.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 600) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      img7: file(relativePath: { eq: "LINKS/texasballettheater.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 600) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      img8: file(relativePath: { eq: "LINKS/edasIsreal.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 600) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      img9: file(relativePath: { eq: "LINKS/leasonEllis.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 600) {
             ...GatsbyImageSharpFluid
@@ -64,11 +99,43 @@ export const SiteImg = ({ altText, imgIndex, height }) => {
       alt={altText}
     />
   )
+  const Img5 = () => (
+    <Img
+      fluid={data.img5.childImageSharp.fluid}
+      style={{ height: `${height}px` }}
+      alt={altText}
+    />
+  )
+  const Img6 = () => (
+    <Img
+      fluid={data.img6.childImageSharp.fluid}
+      style={{ height: `${height}px` }}
+      alt={altText}
+    />
+  )
+  const Img7 = () => (
+    <Img
+      fluid={data.img7.childImageSharp.fluid}
+      style={{ height: `${height}px` }}
+      alt={altText}
+    />
+  )
+  const Img8 = () => (
+    <Img
+      fluid={data.img8.childImageSharp.fluid}
+      style={{ height: `${height}px` }}
+      alt={altText}
+    />
+  )
+  const Img9 = () => (
+    <Img
+      fluid={data.img9.childImageSharp.fluid}
+      style={{ height: `${height}px` }}
+      alt={altText}
+    />
+  )
 
-  //   const Img11 = () => (
-  //     <Img fluid={data.img10.childImageSharp.fluid} style={{ height: `${height}px`}} alt={altText}/>
-  //   )
-  const images = [Img1, Img2, Img3, Img4]
+  const images = [Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8, Img9]
 
   return <>{images[imgIndex]()}</>
 }
--- src/pages/links.js
+++ src/pages/links.js
@@ -1,11 +1,163 @@
 import React from "react"
 import Layout from "../components/layout"
+import { Typography, CardMedia, Card, Link } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
+import { GridContainer, GridItem } from "../components/Grid"
+import GridList from "@material-ui/core/GridList"
+import GridListTile from "@material-ui/core/GridListTile"
+import GridListTileBar from "@material-ui/core/GridListTileBar"
+import IconButton from "@material-ui/core/IconButton"
+import ListSubheader from "@material-ui/core/ListSubheader"
+import Images from "../components/images/linksImages"
+import styled from "styled-components"
+import InfoIcon from "@material-ui/icons/Info"
+import StarBorderIcon from "@material-ui/icons/StarBorder"
 
-export default () => (
-  <Layout>
-    <h1>Links</h1>
-    <p>Welcome to your new Gatsby site.</p>
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    // justifyContent: "space-around",
+    overflow: "hidden",
+    // backgroundColor: theme.palette.background.paper,
+    // height: "100vh",
+  },
+  gridList: {
+    width: 900,
+    // height: "100vh",
+    // height: 600,
+  },
+  titleBar: {
+    background:
+      "linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, " +
+      "rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
+  },
+  icon: {
+    color: "white",
+  },
+}))
 
-    <p>Now go build something great.</p>
-  </Layout>
-)
+const StyledImgCard = styled(Card)`
+  width: 300px;
+  height: 250px;
+  padding: 8px;
+  border-radius: 0;
+  margin: 16px 16px;
+`
+
+const BodyText = ({ children }) => {
+  return (
+    <Typography
+      variant="body1"
+      component="p"
+      gutterBottom="true"
+      style={{ maxWidth: "720px", marginBottom: "16px" }}
+    >
+      {children}
+    </Typography>
+  )
+}
+
+export default function AdvancedGridList() {
+  const classes = useStyles()
+
+  return (
+    <Layout>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Links
+      </Typography>
+
+      <GridContainer>
+        <GridItem>
+          <Link href="https://backstagedancewear.com/">
+            <StyledImgCard>
+              <CardMedia>
+                <Images imgIndex="0" />
+              </CardMedia>
+            </StyledImgCard>
+          </Link>
+        </GridItem>
+        <GridItem>
+          <Link href="http://eudanceart.org/">
+            <StyledImgCard>
+              <CardMedia>
+                <Images imgIndex="2" />
+              </CardMedia>
+            </StyledImgCard>
+          </Link>
+        </GridItem>
+        <GridItem>
+          <Link href="https://www.galveston.com/whattodo/arts/thegrand/">
+            <StyledImgCard>
+              <CardMedia>
+                <Images imgIndex="1" />
+              </CardMedia>
+            </StyledImgCard>
+          </Link>
+        </GridItem>
+        <GridItem>
+          <Link href="http://www.siba-academy.com/">
+            <StyledImgCard>
+              <CardMedia>
+                <Images imgIndex="5" />
+              </CardMedia>
+            </StyledImgCard>
+          </Link>
+        </GridItem>
+        <GridItem>
+          <Link href="https://www.texasballettheater.org/">
+            <StyledImgCard>
+              <CardMedia>
+                <Images imgIndex="6" />
+              </CardMedia>
+            </StyledImgCard>
+          </Link>
+        </GridItem>
+        <GridItem>
+          <Link href="http://eudanceart.org/ISRAEL/israel.htm">
+            <StyledImgCard>
+              <CardMedia>
+                <Images imgIndex="7" />
+              </CardMedia>
+            </StyledImgCard>
+          </Link>
+        </GridItem>
+        <GridItem>
+          <Link href="https://www.leasonellis.com/">
+            <StyledImgCard>
+              <CardMedia>
+                <Images imgIndex="8" />
+              </CardMedia>
+            </StyledImgCard>
+          </Link>
+        </GridItem>
+      </GridContainer>
+      {/* <div className={classes.root}>
+        <GridList cellHeight={120} className={classes.gridList} cols={3}>
+          <GridListTile cols="1"></GridListTile>
+          <GridListTile cols="1">
+            <Images imgIndex="1" />
+          </GridListTile>
+          <GridListTile cols="1">
+            <Images imgIndex="2" />
+          </GridListTile>
+          <GridListTile cols="1">
+            <Images imgIndex="3" />
+          </GridListTile>
+          <GridListTile cols="1">
+            <Images imgIndex="4" />
+          </GridListTile>
+          <GridListTile cols="1">
+            <Images imgIndex="5" />
+          </GridListTile>
+          <GridListTile cols="1">
+            <Images imgIndex="6" />
+          </GridListTile>
+          <GridListTile cols="1">
+            <Images imgIndex="7" />
+          </GridListTile>
+        </GridList>
+      </div> */}
+    </Layout>
+  )
+}
